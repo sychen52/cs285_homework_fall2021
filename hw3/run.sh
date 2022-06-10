eaed4c193dd152c59a11bece2d419cdf42720116
yang <sychen52@gmail.com>
2022-06-09 22:33:25 -0700

    check the finding of hw3

--- hw3/run.sh
+++ hw3/run.sh
@@ -9,11 +9,24 @@
 # ntu 1 ngsptu 100
 # ntu 10 ngsptu 10
 
-ntu=1
-ngsptu=100
-python cs285/scripts/run_hw3_actor_critic.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount \
-       0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --exp_name q5_${ntu}_${ngsptu} -ntu $ntu -ngsptu $ngsptu
+# ntu=1
+# ngsptu=100
+# python cs285/scripts/run_hw3_actor_critic.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount \
+#        0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --exp_name q5_${ntu}_${ngsptu} -ntu $ntu -ngsptu $ngsptu
+# python cs285/scripts/run_hw3_actor_critic.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.90 \
+#        --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_${ntu}_${ngsptu} \
+#        -ntu $ntu -ngsptu $ngsptu
 
-python cs285/scripts/run_hw3_actor_critic.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.90 \
-       --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_${ntu}_${ngsptu} \
-       -ntu $ntu -ngsptu $ngsptu
+# python cs285/scripts/run_hw3_dqn.py --env_name MsPacman-v0 --exp_name q1
+
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_1 --seed 1
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_2 --seed 2
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_3 --seed 3
+
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_doubledqn_1 --double_q --seed 1
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_doubledqn_2 --double_q --seed 2
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_doubledqn_3 --double_q --seed 3
+
+# based on the result, dqn is more stable in terms of random seed
+# doubledqn sometimes has a high reward and then goes back down at the end.
+# Is my doubledqn implementation wrong?
